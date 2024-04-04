28d12b936e421d7745baa1f0b6cb830028c335fe
Dev Ghodasara <ghodasaradev@gmail.com>
2024-04-03 23:36:17 -0400

    Redesigned connect page

--- script.js
+++ script.js
@@ -61,8 +61,65 @@ function displayMenu() {
     }
 }
 
+function clearForm (){
+    document.getElementById('contact-form').reset();
+    autoResize(document.getElementsByName('message')[0]);
+}
+
 function flickerText(){
     body.classList.toggle('alternate')
 }
 
 setInterval(flickerText,2000);
+
+function autoResize(textarea) {
+    // Reset the height to the default in case the content is deleted
+    textarea.style.height = 'auto';
+    
+    // Set the height to the scroll height of the content
+    textarea.style.height = textarea.scrollHeight + 'px';
+}
+
+function submitToGoogleForm(event,form) {
+
+    event.preventDefault();
+
+    var formStatus = document.getElementById("form-status");
+
+    var formData = {
+        timestamp: new Date().toString(),
+        first_name: form.first_name.value,
+        last_name: form.last_name.value,
+        phone_number: form.phone_number.value,
+        email: form.email.value,
+        subject: form.subject.value,
+        message: form.message.value,
+    }
+
+    console.log("Form Data:", formData);
+  
+    fetch('https://script.google.com/macros/s/AKfycbxb61OMdWG_JwGLKNVm1hn79xvFe_z6Xi_57rCTuTL2zAp_pQ3GNk85CuLo3mkdUro/exec',{
+        method: 'POST',
+        body: JSON.stringify(formData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            formStatus.textContent = "There was a problem submitting form data, please retry!";
+            setTimeout(function() {
+                formStatus.textContent = "";
+            }, 2000);
+        }
+        // Display success message
+        formStatus.textContent = "Form submitted successfully!";
+            setTimeout(function() {
+                formStatus.textContent = "";
+            }, 2000);
+        clearForm(); // Clear form after successful submission
+    })
+    .catch(error => {
+        formStatus.textContent = "There was a problem submitting form data, please retry!";
+        setTimeout(function() {
+            formStatus.textContent = "";
+        }, 2000);
+    });
+}
